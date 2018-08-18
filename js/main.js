c58291db08a1f3a47d4b8ae25258021ca10ecc36
Faith Wallace <fwallacephd@gmail.com>
2018-08-18 13:29:49 -0400

    updated presentations and Code for Atlanta

--- js/main.js
+++ js/main.js
@@ -3,12 +3,17 @@ $("#about-me-display").hide();
 $("#projects-display").hide();
 $("#contact-display").hide();
 $("#presentations-display").hide();
+$("#civic-display").hide();
+
 
 //Click on Main Button
 $("#main-display-button").on("click", function(){
+  console.log ("Click")
   $("#main-display").hide();
   $("#about-me-display").show();
   $("#presentations-display").hide();
+  $("#projects-display").hide();
+  
 });
 
 //Click on Projects Button
@@ -16,6 +21,7 @@ $(".projects-button").on("click", function(){
   $("#projects-display").show();
   $("#about-me-display").hide();
   $("#presentations-display").hide();
+  $("#civic-display").hide();
 })
 
 //Click on Presentations Button
@@ -23,6 +29,8 @@ $(".presentation-button").on("click", function(){
   $("#presentations-display").show();
   $("#about-me-display").hide();
   $("#projects-display").hide();
+  $("#civic-display").hide();
+  
 });
 
 //Click on About Me
@@ -31,6 +39,17 @@ $(".about-button").on("click", function(){
   $("#about-me-display").show();
   $("#contact-display").hide();
   $("#presentations-display").hide();
+  $("#civic-display").hide();
+});
+
+//Click on Civic Hack
+$(".civic-button").on("click", function(){
+  $("#civic-display").show();
+  $("#projects-display").hide();
+  $("#about-me-display").hide();
+  $("#contact-display").hide();
+  $("#presentations-display").hide();
+
 });
 
 //Click on Contact Me
@@ -39,6 +58,7 @@ $(".contact-button").on("click", function(){
   $("#projects-display").hide();
   $("#contact-display").show();
    $("#presentations-display").hide();
+   $("#civic-display").hide();
 });
 
 $("#back-button").on("click", function(){
@@ -46,4 +66,5 @@ $("#back-button").on("click", function(){
   $("#about-me-display").show();
   $("#contact-display").hide();
   $("#presentations-display").hide();
+  $("#civic-display").hide();
 });
